80a1aabfd036760bc7b1a8e51a463481f06180a4
theshawa <mrclocktd@gmail.com>
2025-07-08 11:47:30 +0530

    activity logs

--- src/hooks/useConfirmation.ts
+++ src/hooks/useConfirmation.ts
@@ -9,11 +9,13 @@ export const useConfirmation = () => {
     message,
     cancelText = "Cancel",
     confirmText = "Yes",
+    confirmButtonDanger = false,
   }: {
     title: string;
     message: string;
     confirmText?: string;
     cancelText?: string;
+    confirmButtonDanger?: boolean;
   }) => {
     return new Promise<boolean>((resolve) => {
       setConfirmation({
@@ -22,6 +24,7 @@ export const useConfirmation = () => {
         confirmText,
         cancelText,
         open: true,
+        confirmButtonDanger,
         onConfirm: (value: boolean) => {
           resolve(value);
           setConfirmation((prev) => ({ ...prev, open: false }));
@@ -33,6 +36,7 @@ export const useConfirmation = () => {
               confirmText: "Yes",
               cancelText: "Cancel",
               onConfirm: () => {},
+              confirmButtonDanger: false,
             });
           }, 300);
         },
